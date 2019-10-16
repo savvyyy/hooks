bd460eb41a3a49cbe5e8afc911628ea15ea723e8
itadmin <itadmin@lapr101.successivetech.com>
2019-10-16 17:53:29 +0530

    ruby file

---
+++ myfile.rb
@@ -0,0 +1,14 @@
+class Octocat
+    def initialize(name, *breeds) 
+        @name = name
+        @breeds = breeds
+    end
+
+    def display 
+        breed = @breeds.join("-")
+        puts "I m of #{breeds} breed, and my name is #{name}."
+    end
+end
+
+m = Octocat.new("Mona", "cat", "octopus")
+m.display
